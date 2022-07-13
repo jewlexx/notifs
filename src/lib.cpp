fd1a75d36774242e96bfdafa3b828d2c200105a5
Juliette Cordor <grassojames5@gmail.com>
2022-07-13 11:39:15 +1000

    updates

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,1 +1,12 @@
 #include <wintoastlib.h>
+
+using namespace WinToastLib;
+
+static int IsCompatible()
+{
+    return WinToast::isCompatible();
+}
+
+class CustomHandler : public IWinToastHandler
+{
+};
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+extern "C++" {
+     IsCompatible() -> int;
+}
+
+pub unsafe fn is_compatible() -> bool {
+
 }
