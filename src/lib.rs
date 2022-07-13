1754a9ec26c7c64a166d1e877d2780e1a9b86a15
Juliette Cordor <grassojames5@gmail.com>
2022-07-13 11:46:41 +1000

    made function safe

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ extern "C" {
     fn IsCompatible() -> cty::c_int;
 }
 
-pub unsafe fn is_compatible() -> bool {
-    IsCompatible() == 0
+pub fn is_compatible() -> bool {
+    unsafe { IsCompatible() == 0 }
 }
