0d2142877e7d5868d70b2da7414769046fcdf06b
Juliette Cordor <grassojames5@gmail.com>
2022-07-13 11:54:05 +1000

    added dismissal reasons

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+enum WinToastDismissalReason {
+    UserCanceled,
+    ApplicationHidden,
+    TimedOut,
+}
+
 extern "C" {
     fn IsCompatible() -> cty::c_int;
+
 }
 
 pub fn is_compatible() -> bool {
